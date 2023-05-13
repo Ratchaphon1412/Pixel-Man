599a8cdb68e2e8210cc92ca81414662c2c632c8f
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-13 22:27:56 +0700

    create  components logo and navbar

---
+++ components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex flex-row justify-center items-center gap-1 mr-4">
+    <img src="@/assets/images/logo/Blue.svg" class="h-5" />
+    <img src="@/assets/images/logo/Yellow.svg" class="h-5" />
+    <img src="@/assets/images/logo/Green.svg" class="h-5" />
+    <img src="@/assets/images/logo/Red.svg" class="h-5" />
+  </div>
+</template>
