d3d07b9b09cd00e63d44039bccb6ff59e0e13642
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-14 01:23:45 +0700

    edit component logo for resize whrere inherit class name

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="flex flex-row justify-center items-center gap-1 mr-4">
-    <img src="@/assets/images/logo/Blue.svg" class="h-5" />
-    <img src="@/assets/images/logo/Yellow.svg" class="h-5" />
-    <img src="@/assets/images/logo/Green.svg" class="h-5" />
-    <img src="@/assets/images/logo/Red.svg" class="h-5" />
+    <img src="@/assets/images/logo/Blue.svg" :class="$attrs.class" />
+    <img src="@/assets/images/logo/Yellow.svg" :class="$attrs.class" />
+    <img src="@/assets/images/logo/Green.svg" :class="$attrs.class" />
+    <img src="@/assets/images/logo/Red.svg" :class="$attrs.class" />
   </div>
 </template>
+
+<script>
+export default {
+  name: "Logo",
+};
+</script>
