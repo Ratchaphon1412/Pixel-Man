4e92f2b5b7b5e3ae0cd5624c963589570a3fad05
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-14 01:24:21 +0700

    edit navbar bottom login

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -4,7 +4,7 @@
       class="max-w-screen-xl flex flex-wrap items-center justify-between ml-6 p-4"
     >
       <NuxtLink to="/" class="flex flex-row items-center justify-center">
-        <Logo />
+        <Logo class="h-5" />
         <span
           class="self-center text-2xl font-pixelletMedium font-thin whitespace-nowrap text-white"
           >Pixel Man</span
@@ -34,7 +34,7 @@
       </button>
       <div class="hidden w-full md:block md:w-auto" id="navbar-default">
         <ul
-          class="flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-black border-gray-700"
+          class="flex flex-col justify-center items-center p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-black border-gray-700"
         >
           <li>
             <a
@@ -62,15 +62,16 @@
             <a
               href="#"
               class="font-pixellet font-thin block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300"
-              >Pricing</a
+              >Contact</a
             >
           </li>
           <li>
-            <a
-              href="#"
-              class="font-pixellet font-thin block py-2 pl-3 pr-4 text-white rounded md:bg-transparent md:p-0 hover:text-yellow-300"
-              >Contact</a
+            <button
+              type="button"
+              class="text-black font-pixellet bg-[#FFE135] font-thin focus:ring-4 focus:outline-none hover:text-[#3581D8] rounded-lg px-4 py-2 text-center mr-3 md:mr-0"
             >
+              Login
+            </button>
           </li>
         </ul>
       </div>
