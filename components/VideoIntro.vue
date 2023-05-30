b7a26c87da85c736ac50f2dccfcb02808cacc406
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-30 17:10:23 +0700

    add style video css

--- components/VideoIntro.vue
+++ components/VideoIntro.vue
@@ -28,5 +28,9 @@ video {
   width: 100%;
   height: 100%;
   object-fit: cover;
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
