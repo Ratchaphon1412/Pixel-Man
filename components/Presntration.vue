7473d7edbf9fbb7ebcb6411adaa34e34ac54c0c9
ratchaphon1412 <ratchaphon.h111@gmail.com>
2023-05-30 17:19:00 +0700

    create component Presentration

---
+++ components/Presntration.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="w-full h-screen">
+    <video autoplay loop muted playsinline>
+      <source
+        src="https://firebasestorage.googleapis.com/v0/b/pixel-man-97cb5.appspot.com/o/presentration-pixel-man.mp4?alt=media&token=d4536124-5202-4d27-b0ce-a562de602851"
+        type="video/mp4"
+      />
+    </video>
+  </div>
+</template>
